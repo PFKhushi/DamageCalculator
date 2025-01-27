d410bb525a417320d9e896b2176aa1531877f35a
Pedro Firmino <mafiazuka@gmail.com>
2025-01-27 17:27:54 -0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DamageCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SwordDamage swordDamage;
+        public MainWindow()
+        {   
+            swordDamage = new SwordDamage();
+            InitializeComponent();
+            
+        }
+        private void IsFlaming(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.IsChecked == true) 
+            {
+                swordDamage.SetFlame(true);
+            }
+            else
+            {
+                swordDamage.SetFlame(false);
+            }
+            Update();
+            
+        }
+        private void IsMagic(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+
+            if (cb.IsChecked == true)
+            {
+                swordDamage.SetMagic(true);
+            }
+            else
+            {
+                swordDamage.SetMagic(false);
+            }
+            Update();
+        }
+        public void Update() 
+        {
+            Damage.Text = "You rolled " + swordDamage.Roll.ToString() + " for " + swordDamage.Damage.ToString() + "/dmg";
+        }
+
+        private void RollDamage(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetRoll();
+            Update();
+        }
+    }
+}
---
+++ SwordDamage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace DamageCalculator
+{
+    internal class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+        public int Roll;
+        public decimal MagicMultiplier = 1M; 
+        public int FlamingDamage = 0; 
+        public int Damage;
+        Random rand = new Random();
+
+        public void CalculateDamage() 
+        {
+
+            Damage = (int) (Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+
+            
+        }
+        public void SetMagic(bool isMagic) 
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+
+        }
+        public void SetFlame(bool isFlame) 
+        {
+            if (isFlame)
+            {
+                FlamingDamage = FLAME_DAMAGE;
+            }
+            else
+            {
+                FlamingDamage = 0;
+            }
+            CalculateDamage();
+        }
+        public void SetRoll() 
+        {
+            Roll = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Roll += rand.Next(6) + 1;
+            }
+            CalculateDamage();
+        }
+
+    }
+}
